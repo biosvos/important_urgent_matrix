85d828c229406bb7f7946f9f28d6188ed8f5e69f
jinsu <biosvos@gmail.com>
2022-10-16 12:00:31 +0900

    listbox 클래스화

--- main.cpp
+++ main.cpp
@@ -6,50 +6,100 @@
 #include <nana/gui/widgets/listbox.hpp>
 #include <nana/gui/widgets/button.hpp>
 #include <iostream>
+#include <utility>
 
-//void foo(const nana::arg_click &ei) {
-//    nana::form fm(ei.window_handle);
-////    fm.caption(("I am a modal form"));
-////    nana::place place(fm);
-////    place.div("<vert <entry><ba bb>>>");
-//    nana::inputbox::text entry("entry");
-//    nana::inputbox input_box(fm, "description", "ha");
-//    input_box.show();
-////    place["entry"] << input_box;
-////    place.collocate();
-////    std::cout << "Block execution till modal form is closed" << std::endl;
-//    nana::API::modal_window(fm);
-////    std::cout << "modal form is closed" << std::endl;
-//}
+class Items {
+public:
+    void Append(const std::string &str) {
+        items_.push_back(str);
+    }
+
+private:
+    std::vector<std::string> items_;
+};
+
+class ListBox {
+public:
+    explicit ListBox(nana::form &fm, std::string title) : box_(fm) {
+        box_.sortable(false);
+        box_.append_header(std::move(title));
+
+        SetSignals();
+    }
+
+    [[nodiscard]] const nana::listbox &GetReference() const {
+        return box_;
+    }
+
+    void Append(const std::string &str) {
+        box_.at(0).append(str);
+    }
+
+private:
+    nana::listbox box_;
+
+    void SetSignals() {
+        uint64_t selected;
+        box_.events().mouse_down([&]() {
+            auto sel = box_.selected();
+            for (const auto &item: sel) {
+                std::cout << item.item << " " << item.cat << "-> " << std::endl;
+                selected = item.item;
+            }
+        });
+
+        box_.events().mouse_up([&]() {
+            auto ho = box_.hovered(false);
+            if (ho.item == nana::npos) {
+                return;
+            }
+            if (ho.item + 1 == selected) {
+                // 자기 자신이므로 할 이유가 없음
+                return;
+            }
+            if (ho.item == selected) {
+                // 자기 자신이므로 할 이유가 없음
+                return;
+            }
+            std::cout << ho.item << " " << ho.cat << std::endl;
+            if (ho.item + 1 < box_.at(0).size()) {
+                box_.insert_item(nana::listbox::index_pair{0, ho.item + 1}, box_.at(0).at(selected).text(0));
+            } else {
+                box_.at(0).append(box_.at(0).at(selected).text(0));
+            }
+//        std::cout <<  << std::endl;
+            if (selected < ho.item) {
+                box_.erase(box_.at(0).at(selected));
+            } else {
+                box_.erase(box_.at(0).at(selected + 1));
+            }
+//        box.at(0).
+        });
+
+    }
+};
 
 int main() {
     nana::form fm;
     nana::place place(fm);
-    place.div("<vert <list><button>>");
-    nana::listbox box(fm);
-    place["list"] << box;
+    place.div("<vert <<important><urgent>><fit button>>");
+    ListBox important_box(fm, "important");
+    place["important"] << important_box.GetReference();
+    ListBox urgent_box(fm, "urgent");
+    place["urgent"] << urgent_box.GetReference();
     nana::button button(fm, "Add");
     place["button"] << button;
     place.collocate();
-
-    box.events().
-
-    {
-        box.append_header("item");
-        box.at(0).append("aa");
-        box.at(0).append("bb");
-        box.at(0).append("cc");
-    }
 
     button.events().click([&]() {
         nana::inputbox::text entry("entry");
         nana::inputbox input_box(fm, "description", "ha");
         if (input_box.show(entry)) {
             auto value = entry.value();
-            box.at(0).append(value);
+            important_box.Append(value);
+            urgent_box.Append(value);
         }
     });
-
 
     fm.show();
     nana::exec();
